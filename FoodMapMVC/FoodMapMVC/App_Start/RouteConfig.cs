7eab8ef84b7aa96370bb761981445b99b5f41426
Moudou <will.lark.cheng@gmail.com>
2022-03-25 09:48:24 +0800

    美食地圖後台 - 加入列表頁

--- FoodMapMVC/FoodMapMVC/App_Start/RouteConfig.cs
+++ FoodMapMVC/FoodMapMVC/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace FoodMapMVC
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "BackAdmin",
+                url: "BackAdmin/{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
---
+++ FoodMapMVC/FoodMapMVC/Controllers/FoodMapAdminController.cs
@@ -0,0 +1,75 @@
+﻿using FoodMapMVC.Managers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FoodMapMVC.Controllers
+{
+    public class FoodMapAdminController : Controller
+    {
+        private FoodMapManager _mgr = new FoodMapManager();
+
+        // GET: FoodMapAdmin
+        public ActionResult Index()
+        {
+            var list = this._mgr.GetAdminMapList();
+            return View(list);
+        }
+
+        // GET: FoodMapAdmin/Details/5
+        public ActionResult Details(Guid? id)
+        {
+            return View();
+        }
+
+        // POST: FoodMapAdmin/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // POST: FoodMapAdmin/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // POST: FoodMapAdmin/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
